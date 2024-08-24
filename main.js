b13532ada62b32daddaff804c2c757de4c908cae
Johann <johannth88@gmail.com>
2024-08-24 16:37:32 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,116 @@
+import html2canvas from "html2canvas";
+
+// const appEl = document.getElementById('app');
+const mainEl = document.getElementById('pageContent');
+const buttonEl = document.getElementById('takeShot');
+
+
+function animateColumn(ctx, imageData, startX, startY, endX, endY, colWidth, colHeight, duration) {
+  let startTime = null;
+  
+  function draw(timestamp) {
+      if (!startTime) startTime = timestamp;
+      const elapsed = timestamp - startTime;
+
+      // Calculate the current position based on elapsed time
+      const progress = Math.min(elapsed / duration, 1); // Ensure progress doesn't exceed 1
+      const currentX = startX + (endX - startX) * progress;
+      const currentY = startY + (endY - startY) * progress;
+
+      // Clear the canvas and draw the rectangle at the new position
+      ctx.clearRect(startX, startY, colWidth, colHeight + 200); // + 200 since it doesnt clear all the way down?
+
+      // Draw the column at new position
+      ctx.putImageData(imageData, currentX, currentY);
+
+      // Continue animating until the duration is reached
+      if (progress < 1) {
+          requestAnimationFrame(draw);
+      }
+  }
+
+  requestAnimationFrame(draw);
+}
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+const manipulateCanvas = (canvas) => {
+  const ctx = canvas.getContext('2d');
+  const canvasWidth = canvas.offsetWidth;
+  const canvasHeight = canvas.offsetHeight;
+  // How wide should each column be
+  const columnTargetWidth = canvasWidth / 160; // 160 slices in the OG doom
+  const columns = [];
+
+  for(let x = 0; x < canvasWidth - columnTargetWidth; x += columnTargetWidth) {
+    columns.push({ 
+      position: { x, y: 0, width: columnTargetWidth, height: canvasHeight },
+      imageData: ctx.getImageData(x, 0, columnTargetWidth, canvasHeight) });
+  }
+
+  // ToDo: Select image based on link clicked...
+  mainEl.style.backgroundImage = 'url(/d2m1.png)';
+
+  console.log('cols', columns);
+
+  // Mimic Doom's random delay feature, get a first initial delay value, then increase, decrease or stay the same for each column
+  const possibleDelays = Array.from({ length: 10 }, (current, i) => (i + 1) * 100);
+  let baseDelayIndex = getRandomInt(0, possibleDelays.length);
+
+  for (let i = 0; i < columns.length; i++) {
+    // Generate a random delay between 0 and 1000 ms
+    setTimeout(() => {
+      animateColumn(
+        ctx, columns[i].imageData,
+        columns[i].position.x,
+        columns[i].position.y,
+        columns[i].position.x,
+        canvasHeight,
+        columns[i].position.width,
+        columns[i].position.height,
+        1500
+      );
+    }, possibleDelays[baseDelayIndex]);
+    baseDelayIndex = getNextIndex(baseDelayIndex, possibleDelays);
+  }
+}
+
+const getNextIndex = (currentIndex, possibleValues) => {
+  if (currentIndex === 0) {
+    // Either increase or stay the same
+    return Math.random() > 0.5 ? currentIndex += 1 : currentIndex;
+  }
+  if (currentIndex === possibleValues.length - 1) {
+    // Either decrease or stay the same
+    return Math.random() > 0.5 ? currentIndex -= 1 : currentIndex;
+  }
+  const randomVal = Math.random();
+  // Either increase, decrease or stay the same:
+  if (randomVal >= 0 && randomVal <= 0.33) {
+    return currentIndex -= 1;
+  } else  if (randomVal >= 0.33 && randomVal <= 0.66) {
+    return currentIndex;
+  }
+  return currentIndex += 1;
+}
+
+const handleClick = () => {
+  console.log('Hello world');
+  const appWidth = mainEl.offsetWidth;
+  const appHeight = mainEl.offsetHeight;
+  html2canvas(mainEl, { width: appWidth, height: appHeight }).then(function(canvas) {
+    while (mainEl.firstChild) {
+      mainEl.removeChild(mainEl.lastChild);
+    }
+    mainEl.appendChild(canvas);
+    manipulateCanvas(canvas);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  buttonEl.addEventListener('click', handleClick);
+})
---
+++ notes.txt
@@ -0,0 +1,10 @@
+Mission:
+
+On a button, this could be a navigation event in the future, replace the current content, load the next page but cover
+the browser with the canvas screenshot element. Implement a screen melting animation.
+
+Demo:
+
+Generic site with navbar at the top, and big content/hero area as it's pages...
+
+Subject: doom?
